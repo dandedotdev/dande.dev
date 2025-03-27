04e5ae47f9d232803fa132942745b1cddef3b147
dandedotdev <contact@dande.dev>
2025-03-27 20:48:09 +0800

    feat(app): apply `moon_icon` and `disabled` the button

--- app/src/components/header/theme_switcher.rs
+++ app/src/components/header/theme_switcher.rs
@@ -2,22 +2,17 @@
 
 use leptos::prelude::*;
 
+use crate::components::ui::MoonIcon;
+
 #[component]
 pub fn ThemeSwitcher() -> impl IntoView {
     #[rustfmt::skip]
     view! {
 		<div class="flex items-center mr-5">
 			<div class="inline-block relative text-left">
-				<div class="flex justify-center items-center dark:hover:text-primary-400 hover:text-primary-500">
-					<button aria-label="Theme switcher">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							class="size-6 group:hover:text-slate-100"
-						>
-							<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-						</svg>
+				<div class="flex justify-center items-center">
+					<button aria-label="Theme switcher" disabled>
+						<MoonIcon />
 					</button>
 				</div>
 			</div>
